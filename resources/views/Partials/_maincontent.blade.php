767a003c330820c712143311579384ad6c27f5ae
JABER-AHMED <placidcse@gmail.com>
2017-05-07 02:57:12 +0600

    maincontent and sidebar edited and make them single and include in home page

---
+++ resources/views/Partials/_maincontent.blade.php
@@ -0,0 +1,139 @@
+    <div class="col-md-8">
+
+   
+      <!--Card-->
+<div class="card testimonial-card">
+
+    <!--Bacground color-->
+    <div class="card-up default-color-default">
+    <div class="right">
+     <a href=""><h4 class="card-title">C++ is more efficient for ACM programming</h4></a>
+        <h5 class="author">Author: Jaber Ahmed</h5>
+        <p class="time">Today: 3.30pm</p>
+    </div>
+    </div>
+
+    <!--image-->
+    <div class="avatar">
+    <img src="{{asset('images/jaber1.JPG')}}" class="img-circle img-responsive">
+    </div>
+
+    <div class="blockright card-block">
+       
+        <hr>
+        <!--Quotation-->
+        <p>In computer science descrete mathematics is the most important subject.As a computer science student eveyone must have a knowledge about descrete mathematics.</p>
+    </div>
+    <div class="cardfooter">
+      <p><a href="post.html" class="btn btn-default" role = "button">see more...</a></p>
+    </div>
+   <div class="likebutton">
+  <a href=""><span class="fa fa-heart-o" aria-hidden="true"></span></a>
+  <p>46 response</p>
+  <a href=""><span class="fa fa-bookmark" aria-hidden="true"></span></a>
+   </div>
+</div>
+
+
+
+
+<div class="card testimonial-card">
+
+    <!--Bacground color-->
+    <div class="card-up default-color-default">
+    <div class="right">
+      <a href=""><h4 class="card-title">Java programmers are the most creative programmer</h4></a>
+        <h5 class="author">Author: Sudipto Chowdhury</h5>
+        <p class="time">Today: 4.30pm</p>
+    </div>
+    </div>
+
+    <!--image-->
+    <div class="avatar"><img src="{{asset('images/dip.jpg')}}" class="img-circle img-responsive">
+    </div>
+
+    <div class="blockright card-block">
+        
+        <hr>
+        <!--Quotation-->
+        <div class="description">
+        <p>In computer science descrete mathematics is the most important subject.As a computer science student eveyone must have a knowledge about descrete mathematics.</p>
+        </div>
+    </div>
+    <div class="cardfooter">
+      <p><a href="" class="btn btn-default" role = "button">see more...</a></p>
+    </div>
+   <div class="likebutton">
+  <a href=""><span class="fa fa-heart-o" aria-hidden="true"></span></a>
+  <p>46 response</p>
+  <a href=""><span class="fa fa-bookmark" aria-hidden="true"></span></a>
+   </div>
+</div>
+
+
+
+<div class="card testimonial-card">
+
+    <!--Bacground color-->
+    <div class="card-up default-color-default">
+    <div class="right">
+      <a href=""><h4 class="card-title">Descrete Mathematics is called the father of programming</h4></a>
+        <h5 class="author">Author: Tajul Islam Jony</h5>
+        <p class="time">Yeasterday: 8.30pm</p>
+    </div>
+    </div>
+
+    <!--image-->
+    <div class="avatar"><img src="{{asset('images/jony.JPG')}}" class="img-circle img-responsive">
+    </div>
+
+    <div class="blockright card-block">
+        
+        <hr>
+        <!--Quotation-->
+        <p>In computer science descrete mathematics is the most important subject.As a computer science student eveyone must have a knowledge about descrete mathematics.</p>
+    </div>
+    <div class="cardfooter">
+      <p><a href="" class="btn btn-default" role = "button">see more...</a></p>
+    </div>
+   <div class="likebutton">
+  <a href=""><span class="fa fa-heart-o" aria-hidden="true"></span></a>
+  <p>46 response</p>
+  <a href=""><span class="fa fa-bookmark" aria-hidden="true"></span></a>
+   </div>
+</div>
+
+<div class="card testimonial-card">
+
+    <!--Bacground color-->
+    <div class="card-up default-color-default">
+    <div class="right">
+      <a href=""><h4 class="card-title">Data-Structure is more important for programmers or developers</h4></a>
+        <h5 class="author">Author: Mohsin Ahmed</h5>
+        <p class="time">Sunday: 3.30pm</p>
+    </div>
+    </div>
+
+    <!--image-->
+    <div class="avatar">
+    <img src="{{asset('images/mohsin.jpg')}}" class="img-circle img-responsive">
+    </div>
+
+    <div class="blockright card-block">
+        
+        <hr>
+        <!--Quotation-->
+        <p>In computer science descrete mathematics is the most important subject.As a computer science student eveyone must have a knowledge about descrete mathematics.</p>
+    </div>
+    <div class="cardfooter">
+      <p><a href="" class="btn btn-default" role = "button">see more...</a></p>
+    </div>
+   <div class="likebutton">
+  <a href=""><span class="fa fa-heart-o" aria-hidden="true"></span></a>
+  <p>46 response</p>
+  <a href=""><span class="fa fa-bookmark" aria-hidden="true"></span></a>
+   </div>
+</div>
+
+<!--/.Card-->
+    </div>
